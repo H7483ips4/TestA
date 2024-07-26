cebd3a1d8b9f467294ef26b6f0d1ca2bab48d302
H7483ips4 <whizbangtheory54@gmail.com>
2024-07-26 17:48:18 -0500

    App

---
+++ app.py
@@ -0,0 +1,14 @@
+# Import necessary libraries
+from dash import Dash, html
+
+# Create an instance of the Dash class
+app = Dash(__name__)
+
+# Define the layout of the app
+app.layout = html.Div([
+    html.H1("Welcome to the Academic World Dashboard")
+])
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
